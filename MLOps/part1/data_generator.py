a12f90e52f692df1fa2f7a988961b9d0076f89a8
노하일 <rohhail@nohail-ui-MacBookPro.local>
2024-09-01 16:56:47 +0900

    mlops

---
+++ MLOps/part1/data_generator.py
@@ -0,0 +1,80 @@
+# data_generator.py
+import time
+from argparse import ArgumentParser
+
+import pandas as pd
+import psycopg2
+from sklearn.datasets import load_iris
+
+# Iris 데이터셋을 불러옴
+#PostgreSQL 테이블에 적합하게 변경
+def get_data():
+    X, y = load_iris(return_X_y=True, as_frame=True)
+    df = pd.concat([X, y], axis="columns")
+    rename_rule = {
+        "sepal length (cm)": "sepal_length",
+        "sepal width (cm)": "sepal_width",
+        "petal length (cm)": "petal_length",
+        "petal width (cm)": "petal_width",
+    }
+    df = df.rename(columns=rename_rule)
+    return df
+
+#iris_data 테이블이 존재하지 않는 경우 테이블을 생성
+def create_table(db_connect):
+    create_table_query = """
+    CREATE TABLE IF NOT EXISTS iris_data (
+        id SERIAL PRIMARY KEY,
+        timestamp timestamp,
+        sepal_length float8,
+        sepal_width float8,
+        petal_length float8,
+        petal_width float8,
+        target int
+    );"""
+    print(create_table_query)
+    with db_connect.cursor() as cur:
+        cur.execute(create_table_query)
+        db_connect.commit()
+
+#주어진 데이터를 iris_data 테이블에 삽입
+def insert_data(db_connect, data):
+    insert_row_query = f"""
+    INSERT INTO iris_data
+        (timestamp, sepal_length, sepal_width, petal_length, petal_width, target)
+        VALUES (
+            NOW(),
+            {data.sepal_length},
+            {data.sepal_width},
+            {data.petal_length},
+            {data.petal_width},
+            {data.target}
+        );
+    """
+    print(insert_row_query)
+    with db_connect.cursor() as cur:
+        cur.execute(insert_row_query)
+        db_connect.commit()
+
+#무한 루프를 통해 Iris 데이터 프레임에서 무작위로 한 행을 추출하여 insert_data 함수를 호출
+def generate_data(db_connect, df):
+    while True:
+        insert_data(db_connect, df.sample(1).squeeze())
+        time.sleep(1)
+
+
+if __name__ == "__main__":
+    parser = ArgumentParser()
+    parser.add_argument("--db-host", dest="db_host", type=str, default="localhost")
+    args = parser.parse_args()
+
+    db_connect = psycopg2.connect(
+        user="myuser",
+        password="mypassword",
+        host=args.db_host,
+        port=5432,
+        database="mydatabase",
+    )
+    create_table(db_connect)
+    df = get_data()
+    generate_data(db_connect, df)
